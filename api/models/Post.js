e5618551e2f666317608f24e9becedeaabbeccdd
Tom Wood <dexterbyte01@gmail.com>
2024-02-14 14:52:25 +0000

    alter search query to desensitize case

--- api/models/Post.js
+++ api/models/Post.js
@@ -31,7 +31,7 @@ class Post {
 
     static async getPostByContent(data) {
         const searchString = data
-        const response = await db.query("SELECT post.*, account.username AS author_username FROM post INNER JOIN account ON post.account_id = account.account_id WHERE post.content LIKE '%' || $1 || '%' OR post.title LIKE '%' || $1 || '%';", [searchString])
+        const response = await db.query("SELECT post.*, account.username AS author_username FROM post INNER JOIN account ON post.account_id = account.account_id WHERE LOWER(post.content) LIKE LOWER('%' || $1 || '%') OR LOWER(post.title) LIKE LOWER('%' || $1 || '%');", [searchString])
         if(response.rows.length === 0){
             throw new Error("No posts found matching your search.")
         }
